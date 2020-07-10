2addc4d768f2ae441206f5120036c9be9b1d7b43
Jeremy Park <seungilpark88@gmail.com>
2020-07-09 23:56:01 -0700

    new year chaos started

---
+++ New_Year_Chaos.js
@@ -0,0 +1,12 @@
+// 
+// input is an array of integers 
+// initial state is = 1,2,3,4,5,...
+// when bribes take place, the number swaps position
+// one person can bribe maximum twice
+//Print an integer denoting the minimum number of bribes needed to get the queue into its final state.
+//Print Too chaotic if the state is invalid, i.e. it requires a person to have bribed more than
+
+
+function minimumBribes(q) {
+    // 
+} 
